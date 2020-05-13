42e78624af2caff7538009dc91f6505f34f1bba1
Jonasebrother <57625401+Jonasebrother@users.noreply.github.com>
2020-05-13 12:15:44 +0200

    Update availability.groovy

--- availability.groovy
+++ availability.groovy
@@ -6,14 +6,65 @@ def fittingRooms = [] // list for all rooms fullfilling the conditions
 // go through all rooms...
 for (roomNumber in rooms) {
     def room = getServiceReponse('/rooms/'+roomNumber)
-    // is room cheaper than 100?
+    print(room)
+    // Get all free rooms in their category
+    // def freeEinzelRooms = []
+    // def freeDoppelRoom = []
+    // def freeSuitRoom = []
+    // if status = free
+    //      if roomtype = single
+    //          room['number'] = roomNuber
+    //          freeEinzelRooms.add(room)
+    //      if roomtype = double
+    //          room['number'] = roomNuber
+    //          freeDoppelRooms.add(room)
+    //      if roomtype = suite
+    //          room['number'] = roomNuber
+    //          freeSuiteRooms.add(room)
     if (room.get('price') < 100) {
         room['number'] = roomNumber // add room number to room info
         fittingRooms.add(room) 
-    }
+    } 
+
+    //execution.setVariable("number", fittingRooms[0]['number']+", "+fittingRooms[1]['number'])
+    // number: 100
+    // number: "100, 202, 310"
+    // type: "single, double, suite"
+    // price: "80€ + 120€ + 300€ = 500€"
+
+
+    // Ende:
+    // return Dopplezimmer, Einzelzimmer
+    // Camunda Modeler (festes) Form: 
+    // - Raumnummer: DefaultValue: ${number} 
+    // - Typ: DefaultValue: ${roomtype} 
+    // - Preis: DefaultValue: ${price} 
+    //
+    // Generated Form:
+    // Array mit Räumen (rooms)
+    // <html>
+    // <script>
+    //  for room in rooms {
+    //      Generiere das einzelne Form
+    //      ODER
+    //      Generiere Tabelle mit Buttons
+    //  }
+    // </script>
+    // <form>
+    //  <input>...
+    //  <button>...
+    // </form>
+    // </html>
+
+
+    // Irgendein anderes Skript:
+    // Input: number ("100, 202, 310")
+    // Verarbeitung: rooms = number.split(",");
+    //                 -> ["100", "202", "310"]
 }
 
-print(fittingRooms) // [[number:100, price:80, ...], [number:101, price:80, ...], ...]
+
+//print(fittingRooms) // [[number:100, price:80, ...], [number:101, price:80, ...], ...]
 
 /* Set vars for Camunda BPM
 execution.setVariable("number", fittingRooms[0]['number'])
