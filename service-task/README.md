676289daf039228e3eb38ec1e9272b8ce99ab59c
strumswell <philipp.bukkit@gmail.com>
2020-05-29 12:17:40 +0200

    updated service for service tasks

---
+++ service-task/README.md
@@ -0,0 +1,12 @@
+# How to run the service
+## Requirements
+- Node v12.16.3
+- npm
+
+## Run the service
+- Open the index.js and change the baseUrl to your local Camunda BPM server adress here: `const config = { baseUrl: 'http://bolte.cloud:8080/engine-rest/', use: logger };` Make sure to keep the `/engine-rest/` part!
+- Open your terminal
+    - Make sure you are in the same directory as the index.js. If not, use `cd`to change your directory
+    - Run `npm install` to install the dependencies.
+    - Run `node index.js` to start the service. Keep your terminal open.
+    - If you want to stop the service, use CTRL-C and close your terminal
--- service-task/index.js
+++ service-task/index.js
@@ -6,34 +6,25 @@ const { Client, Variables, logger } = require('camunda-external-task-client-js')
 const config = { baseUrl: 'http://bolte.cloud:8080/engine-rest/', use: logger };
 const client = new Client(config);
 
-client.subscribe('bonitaet', async function({ task, taskService }) {
-    const vorname = task.variables.get('FormField_191imfb');
-    const name = task.variables.get('FormField_0hrk5hs');
-    const iban = task.variables.get('FormField_2n4p8ss');
-    console.log("Incoming: " + vorname + " " + name + " - " + iban);
+client.subscribe('bonitaetPruefen', async function({ task, taskService }) {
+    const vorname = task.variables.get('kA_vorname');
+    const name = task.variables.get('kA_nachname');
+    const mail = task.variables.get('kA_eMail');
+    console.log("Incoming: " + vorname + " " + name + " - " + mail);
     let bonitaet = false;
     
     // if last char of IBAN is smaller 6 
-    if (parseInt(iban.slice(-1)) < 6) {
+    if (vorname == "Jens") {
         console.log("Bonität da!");
         bonitaet = true;
     }
 
-    console.log("Bonität:" + bonitaet)
     const camunda_bonitaet = new Variables();
     camunda_bonitaet.set("bonitaet", bonitaet);
 
     await taskService.complete(task, camunda_bonitaet);
 })
 
-/*  Output of tests
-*   ===========================================================
-*   ✓ subscribed to topic bonitaet
-*   Incoming: Klaus Kleber - DE26500105178418388875
-*   Bonität da!
-*   Bonität:true
-*   ✓ completed task d37a1d7f-9c30-11ea-8ee0-87af75601f00
-*   Incoming: Klaus Kleber - DE26500105178418388876
-*   Bonität:false
-*   ✓ completed task b2f2843b-9c31-11ea-8ee0-87af75601f00
-*/
+client.subscribe('rechnungGenerieren', async function({ task, taskService }) {
+    await taskService.complete(task);
+})
