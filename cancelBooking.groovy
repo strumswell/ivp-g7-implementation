26c532c291ce1b86dac716f6df477fe3ab0fa206
strumswell <philipp.bukkit@gmail.com>
2020-07-16 15:35:18 +0200

    ✨added cancel booking script

---
+++ cancelBooking.groovy
@@ -0,0 +1,44 @@
+import groovy.json.JsonSlurper
+
+def bookingnr = execution.getVariable("sE_Buchungsnummer")
+def bookedRooms = getServiceReponse('buchung/'+bookingnr)
+
+bookedRooms.each { stadt, hotelids -> 
+    hotelids.each { hotelid, allRooms ->
+        allRooms.rooms.each { roomID ->
+            updateRoomInfo(stadt+"/"+hotelid+"/rooms/"+roomID, '{"status": "free"}')
+            println("Cancelled " + hotelid + " " + roomID)
+        }
+    }
+}
+
+/**
+* Change a room's infos
+* @param route the route to call
+* @param data json of data to change
+*/
+def updateRoomInfo(route, data) {
+    def connection = new URL('http://implproz.cbu.net:8007/hotels/'+route).openConnection();
+    connection.setRequestMethod("PUT")
+    connection.setDoOutput(true)
+    connection.setRequestProperty("Content-Type", "application/json")
+    connection.getOutputStream().write(data.getBytes("UTF-8"))
+    if(connection.getResponseCode().equals(200)) {
+        println(connection.getInputStream().getText())
+    }
+    return
+}
+
+/**
+* Make a call to the rooms service api.
+* @param route the route to call
+* @return the response of the service api
+*/
+def getServiceReponse(route) {
+    def connection = new URL('http://implproz.cbu.net:8007/'+route).openConnection();
+    def response = '';
+    if(connection.getResponseCode().equals(200)) {
+        response = connection.getInputStream().getText()
+    }
+    return new JsonSlurper().parseText(response)
+} 
