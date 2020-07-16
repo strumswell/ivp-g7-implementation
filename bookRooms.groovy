9ad608633804a356490f3d7836a8abb53d1e2626
strumswell <philipp.bukkit@gmail.com>
2020-07-16 13:01:53 +0200

    ✨added room booking script

---
+++ bookRooms.groovy
@@ -0,0 +1,31 @@
+def lockedRooms = execution.getVariable("vP_blockierteRäume")
+def city = execution.getVariable("kE_stadtAuswahl")
+def hotelid = execution.getVariable("kE_hotelAuswahl")
+def firstname = execution.getVariable("kA_vorname")
+def lastname = execution.getVariable("kA_nachname")
+def arrival = Date.parse("dd.MM.yyyy", execution.getVariable("kE_anreisedatum"))
+def depart = Date.parse("dd.MM.yyyy", execution.getVariable("kE_abreisedatum"))
+lockedRooms = lockedRooms.split(',')
+
+for (roomNumber in lockedRooms) {
+    // Book room
+    updateRoomInfo(city+'/'+hotelid+'/rooms/'+roomNumber, '{"status": "occupied", "guest": "'+firstname+' '+lastname+'" ,"bookedfrom": "'+arrival.getDateString()+'", "bookeduntil": "'+depart.getDateString()+'"}')
+    println('Booked '+roomNumber)
+}
+
+/**
+* Change a room's infos
+* @param route the route to call
+* @param data json of data to change
+*/
+def updateRoomInfo(route, data) {
+    def connection = new URL('http://implproz.cbu.net:8007/hotels/'+route).openConnection();
+    connection.setRequestMethod("PUT")
+    connection.setDoOutput(true)
+    connection.setRequestProperty("Content-Type", "application/json")
+    connection.getOutputStream().write(data.getBytes("UTF-8"))
+    if(connection.getResponseCode().equals(200)) {
+        println(connection.getInputStream().getText())
+    }
+    return
+}
