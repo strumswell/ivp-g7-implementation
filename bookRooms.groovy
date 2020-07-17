4a01c4d52147a5fa0ccb66914c9ab937528e470f
strumswell <philipp.bukkit@gmail.com>
2020-07-17 17:46:42 +0200

    🐛 changed ä to ae

--- bookRooms.groovy
+++ bookRooms.groovy
@@ -1,4 +1,4 @@
-def lockedRooms = execution.getVariable("vP_blockierteRäume")
+def lockedRooms = execution.getVariable("vP_blockierteRaeume")
 def city = execution.getVariable("kE_stadtAuswahl")
 def hotelid = execution.getVariable("kE_hotelAuswahl")
 def firstname = execution.getVariable("kA_vorname")
